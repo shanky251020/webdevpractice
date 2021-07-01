eeca105af1ce2e2b729eed9dce77c48cdf1fdda8
shanky251020 <shashank16septd@gmail.com>
2021-07-01 20:56:01 +0530

    Added different methods of object creation

--- Js practice /script1.js
+++ Js practice /script1.js
@@ -2,7 +2,7 @@
 
 // allows to create objects without defining the class 
 
-// This type of object creation is called JSON --- Javascript object Notation
+/* This type of object creation is called JSON --- Javascript object Notation
 var bird={
     x:100,
     y:200,
@@ -33,3 +33,37 @@ bird.eggs.forEach(function(val,idx) // This tells the index of every element of 
 {
     console.log(val,idx);
 });
+
+*/
+// This is another way of creating function 
+
+function Fruit(color,taste)
+{
+    this.color = color;
+    this.taste = taste;
+}
+
+var orange = new Fruit("Red","sour");
+
+// Another way Ecmascript 2015 -- class keyword
+/*let BMW = new carclass("xyz",1000);
+let pulsar = new bikeclass("ABC",1000) classes are not hoisted as functions */
+
+class carclass{
+    constructor(model,price)
+    {
+        this.model = model;
+        this.price = price;
+    }
+}
+
+let BMW = new carclass("xyz",1000);
+
+let bikeclass = class{
+    constructor(model,price)
+    {
+        this.model = model;
+        this.price = price;
+    }
+}
+let pulsar = new bikeclass("ABC",1000)

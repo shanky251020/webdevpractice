0b4c236fd7cf654457a3590c16f44442686e9b26
shanky251020 <shashank16septd@gmail.com>
2021-07-06 19:26:17 +0530

    Added DOM traversal

---
+++ DOM-manip/DomTraversal/script.js
@@ -0,0 +1,18 @@
+// Now DOM traversal 
+
+// First go onto Html and click on any element and use it in console or store it in
+// We have to go from men to duo steps :- 
+
+/* let men = temp1
+men.parentElement
+men.parentElement.previousElementSibling
+men.parentElement.previousElementSibling.previousElementSibling
+men.parentElement.previousElementSibling.previousElementSibling.children[1]
+Now we get duo 
+*/
+
+//Now we have to remove some element and place it on anywhere
+// Remember DOM object is not deleted but it removes from the page
+// men.parentElement.remove()
+// qua.appendChild(men.parentElement)
+

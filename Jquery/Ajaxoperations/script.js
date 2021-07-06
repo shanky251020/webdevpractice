fe8df6325a90524fd1b5593a7b7dbffb571369bb
shanky251020 <shashank16septd@gmail.com>
2021-07-07 01:38:11 +0530

    Added ajax operation jquery

---
+++ Jquery/Ajaxoperations/script.js
@@ -0,0 +1,16 @@
+
+$(()=>{ 
+let list = $('#people')
+$('#fetch').click(function(){
+$.get(`https://reqres.in/api/users?page=2`,(data)=>{     // for asynchronous call back function
+for(let p of data.data)
+{
+    list.append(
+        $(`<li>${p.first_name}</li>`)
+    )
+}
+})
+})
+
+
+})

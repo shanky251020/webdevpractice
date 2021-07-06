a0db4d370d5ad2abc9656d83fd1d7fa60cf13fef
shanky251020 <shashank16septd@gmail.com>
2021-07-06 22:24:18 +0530

    Added code for adding and removing item from todo

--- DOM-manip/sampletodolist/script.js
+++ DOM-manip/sampletodolist/script.js
@@ -15,5 +15,14 @@ addtask.onclick = function()
     let li = document.createElement('li')
     li.innerText = newtask.value
     todolist.appendChild(li)
+
+    let xbutton = document.createElement('button')  // lets create a remove element button
+    xbutton.onclick = function(event)  // It takes a event 
+    {
+       console.log(event)    // We can check event by clicking on the xbutton
+       event.target.parentElement.remove()  // This will remove the list item 
+    }
+    xbutton.innerText = '❌'
+    li.appendChild(xbutton)
 }
 }

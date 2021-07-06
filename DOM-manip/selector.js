90d158fa3ba51cf09e2941753862ad767e680461
shanky251020 <shashank16septd@gmail.com>
2021-07-06 17:44:29 +0530

    Added Dom Selector

---
+++ DOM-manip/selector.js
@@ -0,0 +1,3 @@
+document;  // It returns the whole document 
+document.getElementById('p1')    // It gives the element by the mentioning id
+document.getElementsByClassName('para')  // It gives the element by the classname
